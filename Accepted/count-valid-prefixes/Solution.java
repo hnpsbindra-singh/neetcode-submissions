92bb87aadd09e8c33c09c68cae368595685cb0f8
Harnimarpreet Singh Bindra <hnps.bindra@gmail.com>
2026-08-20 21:19:30 +0530

    Sync Solution: count-valid-prefixes

---
+++ Accepted/count-valid-prefixes/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countValidPrefixes(String s) {
+        int zero = 0;
+        int one = 0;
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i)-'0'==0){
+                zero++;
+            }else {
+                one++;
+            }
+            if (Math.abs(zero-one)<=1){
+                count++;
+            }
+        }
+        System.gc();
+        return count;
+    }
+}
